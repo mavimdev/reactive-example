18a6a2c544545edd3d2de0aa0ddfa98793cf8885
Avim <carlos.avim@vodafone.com>
2021-05-07 14:13:11 +0100

    returning an infinite stream of Foo objects

--- src/main/java/com/mavim/reactive/service/FooService.java
+++ src/main/java/com/mavim/reactive/service/FooService.java
@@ -5,17 +5,13 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
-import java.util.stream.Collectors;
-import java.util.stream.LongStream;
 
 @Service
 public class FooService {
 
     public Flux<Foo> getAllFoo() {
-        return Flux.fromIterable(
-                LongStream.rangeClosed(0, 20)
-                        .mapToObj(i -> new Foo(i, "Foo" + i))
-                        .collect(Collectors.toList())
-        ).delayElements(Duration.ofSeconds(1));
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(i -> new Foo(i, "Foo" + i));
     }
+
 }
